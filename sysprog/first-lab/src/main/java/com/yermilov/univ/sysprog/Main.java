802aecf1da3b477dcec86a27b0e9bc840b6530bb
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2018-09-19 10:34:44 +0300

    small fix

--- sysprog/first-lab/src/main/java/com/yermilov/univ/sysprog/Main.java
+++ sysprog/first-lab/src/main/java/com/yermilov/univ/sysprog/Main.java
@@ -5,6 +5,7 @@ import com.yermilov.univ.sysprog.helpers.Messages;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static com.yermilov.univ.sysprog.helpers.Messages.HELP_MESSAGE;
 
@@ -30,6 +31,14 @@ public class Main {
         }
         if (args.length > 1) {
             System.out.println(Messages.WRONG_USAGE_MESSAGE);
+            return;
+        }
+        System.out.println("Please, write path to file");
+        Scanner scanner = new Scanner(System.in);
+        path = scanner.nextLine();
+        File file = new File(path);
+        if (!file.exists()) {
+            System.out.println(Messages.FILE_NOT_EXISTS_MESSAGE);
             return;
         }
         Worker worker = new Worker();
