401fcc665a9a7b4bd6c1f5e04a4266ff4a6e8bf7
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2018-11-10 22:06:22 +0200

    Add almost all from first lab

---
+++ marinich/src/main/scala/Number.scala
@@ -0,0 +1,355 @@
+class Number(var value: String, var sign: Char) {
+  private val zeroCharCode: Int = '0'
+
+  def length = value.length
+
+  def max(a: Int, b: Int) = Math.max(a, b)
+
+  def cleanLeadingZeroes(s: String): String = {
+    if (s.endsWith("0") && s.length > 1) cleanLeadingZeroes(s.substring(0, s.length - 1))
+    else s
+  }
+
+  def +(n: Number): Number = {
+    if (n.sign == this.sign) {
+      new Number(absAdd(n).value, this.sign)
+    }
+    else {
+      if (this.sign == '+')
+        this - Number(n.value, '+')
+      else
+        n - Number(this.value, '+')
+    }
+  }
+
+  def absAdd(n: Number): Number = {
+    val res: StringBuilder = new StringBuilder
+    var carry = 0
+    for (i <- 0 to max(this.length, n.length)) {
+      val addition = (if (this.length > i) this.value(i) - zeroCharCode else 0) + (if (n.length > i) n.value(i) - zeroCharCode else 0) + carry
+      carry = addition / 10
+      res.append(addition % 10)
+    }
+    Number(cleanLeadingZeroes(res.toString), sign)
+  }
+
+  def absSubtract(n: Number): Number = {
+    val res: StringBuilder = new StringBuilder
+    var carry = 0
+    val iTo = max(this.length, n.length)
+    for (i <- 0 to iTo) {
+      var subtraction = (if (this.length > i) this.value(i) - zeroCharCode else 0) - (if (n.length > i) n.value(i) - zeroCharCode else 0) + carry
+      if (subtraction < 0) {
+        carry = -1
+        subtraction = subtraction + 10
+      }
+      else carry = 0
+      res.append(subtraction % 10)
+    }
+
+    Number(cleanLeadingZeroes(res.toString), sign)
+  }
+
+  def -(n: Number): Number = {
+    if (this.sign == n.sign) {
+      if (this.abs() > n.abs) {
+        val number = absSubtract(n)
+        Number(number.value, this.sign)
+      }
+      else {
+        if (this == n) {
+          Number(0)
+        }
+        else {
+          if (this.sign == '+')
+            Number((n absSubtract this).value, '-')
+          else
+            Number((n absSubtract this).value, '+')
+        }
+      }
+    }
+    else {
+      if (this.abs > n.abs) {
+        val number = absAdd(n)
+        Number(number.value, this.sign)
+
+      }
+      else {
+        if (this == n) {
+          2 * this
+        }
+        else {
+          if (this.sign == '+')
+            Number((n absAdd this).value, '+')
+          else
+            Number((this absAdd n).value, '-')
+        }
+      }
+    }
+  }
+
+  def abs(): Number = {
+    Number(this.value, '+')
+  }
+
+  def /(n: Number): (Number, Number) = {
+    if (n > this) (Number(0), this)
+    else {
+      var divider = n.abs()
+      var resDiv = new StringBuilder
+      var a = value.reverse
+      var car = Number(0)
+      var num = Number(a.substring(0, divider.length).reverse, '+')
+      for (i <- divider.length to a.length) {
+        var b = Number(0)
+        var j = 0
+        while (j <= 9 && (divider * Number(j + 1) <= num)) {
+          j = j + 1
+        }
+        if (Number(j + 1) * divider > num) {
+          b = Number(j) * divider
+          resDiv.append(j)
+        }
+        car = num - b
+        num = car * Number(10) + Number(if (i < a.length) a(i).toString else "0", '+')
+      }
+      var sign = if (this.sign == n.sign) '+' else '-'
+      (Number(resDiv.toString.reverse, sign), car)
+    }
+  }
+
+  def *(n: Number): Number = {
+    var res1 = Number(0)
+    var res2 = Number(0)
+    if (this.length < n.length) n * this
+    else {
+      for (i <- 0 until n.length) {
+        res2 = this multiplyByDigit (n.value(i) - '0')
+        for (j <- 0 until i) {
+          res2.multiplateByTen()
+        }
+        res1 = res1 + res2
+      }
+      val signOfResult = if (this.sign == n.sign) '+' else '-'
+      Number(res1.value, signOfResult)
+    }
+  }
+
+  def multiplyByDigit(num: Int): Number = {
+    var carry = 0
+    var res = new StringBuilder()
+    for (i <- 0 to this.length) {
+      val resMult = ((if (i < this.length) this.value(i) else '0') - '0') * num + carry
+      res append (resMult % 10)
+      carry = resMult / 10
+    }
+    Number(res.toString, this.sign)
+  }
+
+  def <(n: Number): Boolean = {
+    !(this > n)
+  }
+
+  def >(n: Number): Boolean = {
+    if (this == n) false
+    else {
+      if (this.sign != n.sign) {
+        this.sign == '+'
+      }
+      else {
+        if (this.length > n.length) {
+          this.sign == '+'
+        }
+        else {
+          if (this.length < n.length) {
+            !(this.sign == '+')
+          }
+          else {
+            (this.value.reverse > n.value.reverse) ^ (this.sign == '-')
+          }
+        }
+      }
+    }
+  }
+
+
+  def ==(n: Number): Boolean = {
+    equals(n)
+  }
+
+  def !=(n: Number): Boolean = {
+    !(this == n)
+  }
+
+  def <=(n: Number): Boolean = {
+    this == n || this < n
+  }
+
+  def >=(n: Number): Boolean = {
+    this == n || this > n
+  }
+
+  def +%(n: Number, mod: Number) = {
+    (this + n) % mod
+  }
+
+  def -%(n: Number, mod: Number) = {
+    (this - n) % mod
+  }
+
+  def /%(n: Number, mod: Number) = {
+    (this + n) / mod
+  }
+
+  def *%(n: Number, mod: Number) = {
+    (this * n) % mod
+  }
+
+  def %(n: Number): Number = {
+    (this / n)._2
+  }
+
+  def ^(n: Number): Number = {
+    if (n == 0) {
+      1
+    }
+    else {
+      if (n == 1) {
+        this
+      }
+      else if (n == 2) {
+        this * this
+      }
+      else {
+        (this ^ (n div 2) ^ 2) * (this ^ (n % 2))
+      }
+    }
+  }
+
+  def ^%(n: Number, mod: Number): Number = {
+    (this ^ n) % mod
+  }
+
+  def sqrt(): Number = {
+    var res = Number(1)
+    // ^ Number(this.length)
+    var decreased = false
+    var newRes = Number(1)
+    while (true) {
+      newRes = res + (this div res) div 2
+      if ((decreased && res < newRes) || res == newRes) return res
+      decreased = newRes < res
+      res = newRes
+    }
+    res
+  }
+
+  def sqrtMod(mod: Number): Number = {
+    sqrt % mod
+  }
+
+  def div(n: Number): Number = {
+    (this / n)._1
+  }
+
+  override def toString: String = (if (this.sign == '-') this.sign else "") + this.value.reverse
+
+  override def equals(obj: scala.Any): Boolean = obj match {
+    case num: Number => if (this.sign == num.sign && this.value == num.value) true else false
+    case n: Long => equals(Number(n))
+    case n: Int => equals(Number(n))
+    case _ => false
+  }
+
+  def unary_- = Number(this.value, if (this.sign == '+' && this.value != "0") '-' else '+')
+
+  private def multiplateByTen(): Unit = {
+    this.value = "0" + this.value
+  }
+
+}
+
+object Number extends App {
+  val ZERO = Number(0)
+
+  def apply(int: Int): Number = {
+    if (int == 0)
+      new Number("0", '+')
+    else {
+      var absI = Math.abs(int)
+      val s: StringBuilder = new StringBuilder
+      while (absI > 0) {
+        s.append(absI % 10)
+        absI = absI / 10
+      }
+      new Number(s.toString(), if (int >= 0) '+' else '-')
+    }
+  }
+
+  def apply(int: Long): Number = {
+    if (int == 0)
+      new Number("0", '+')
+    else {
+      var absI = Math.abs(int)
+      val s: StringBuilder = new StringBuilder
+      while (absI > 0) {
+        s.append(absI % 10)
+        absI = absI / 10
+      }
+      new Number(s.toString(), if (int >= 0) '+' else '-')
+    }
+  }
+
+  def cleanLeadingZeroes(s: String): String = {
+    if (s.endsWith("0") && s.length > 1) cleanLeadingZeroes(s.substring(0, s.length - 1))
+    else s
+  }
+
+  def apply(value: String, sign: Char) = new Number(cleanLeadingZeroes(value), sign)
+
+  def apply(number: Number): Number = number
+
+  def max(a: Number, b: Number): Number = if (a > b) a else b
+
+  implicit def intToNumber(n: Int): Number = Number(n)
+
+  /**
+    * Solve equations like a*x === b(mod m)
+    *
+    * @param a
+    * @param b
+    * @param m
+    * @return x
+    */
+  def solveEquation(a: Number, b: Number, m: Number): Option[Number] = {
+    var (g, x1, y1) = gcdExtended(a.abs(), m.abs())
+    println(g + " " + x1 + " " + y1)
+    if (b % g == 0) {
+      x1 *= b div g
+      y1 *= b div g
+      if (a < 0) x1 *= -1
+      if (m < 0) y1 *= -1
+      Some(x1)
+    }
+    else None
+  }
+
+  def gcd(a: Number, b: Number): Number = {
+    if (b == Number(0))
+      a
+    else
+      gcd(b, a % b)
+  }
+
+  def gcdExtended(a: Number, b: Number): (Number, Number, Number) = {
+    if (a == Number(0)) {
+      (b, Number(0), Number(1))
+    }
+    else {
+      val (d, x1, y1) = gcdExtended(b % a, a)
+      (d, y1 - ((b div a) * x1), x1)
+    }
+  }
+
+  println(solveEquation(111, 75, 321))
+}
---
+++ marinich/src/test/scala/NumbersSpec.scala
@@ -0,0 +1,347 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class NumbersSpec extends FlatSpec with Matchers {
+
+  "+" should
+    "7 + 6  == 13" in {
+    val first = 7
+    val second = 6
+    (Number(first) + Number(second)) should be(Number(first + second))
+  }
+  it should " -7 + -6 == -13" in {
+    val first = 7
+    val second = 6
+    (Number(first) + Number(second)) should be(Number(first + second))
+  }
+
+  it should " -7 + 6 == -1" in {
+    val first = -7
+    val second = 6
+    (Number(first) + Number(second)) should be(Number(first + second))
+  }
+
+  it should " 7 + -6 == 1" in {
+    val first = 7
+    val second = -6
+    (Number(first) + Number(second)) should be(Number(first + second))
+  }
+
+  it should "pentest" in {
+    for (i <- 5000 to 5000) {
+      for (j <- -5000 to 5000)
+        Number(i) + Number(j) should be(Number(i + j))
+    }
+  }
+
+
+  ">" should
+    "7>1" in {
+    val first = 7
+    val second = 1
+    Number(first) > Number(second) should be(true)
+  }
+  it should "173 > 34" in {
+    val first = 173
+    val second = 34
+    Number(first) > Number(second) should be(true)
+  }
+
+  it should "173 > 171" in {
+    val first = 173
+    val second = 171
+    Number(first) > Number(second) should be(true)
+  }
+
+  it should "173 > -34" in {
+    val first = 173
+    val second = -34
+    Number(first) > Number(second) should be(true)
+  }
+
+  it should "-173 > 34" in {
+    val first = -173
+    val second = 34
+    Number(first) > Number(second) should be(false)
+  }
+
+  it should "-7>-1" in {
+    val first = -7
+    val second = -1
+    Number(first) > Number(second) should be(false)
+  }
+  it should "-173 > -34" in {
+    val first = -173
+    val second = -34
+    Number(first) > Number(second) should be(false)
+  }
+
+  it should "-173 > -171" in {
+    val first = -173
+    val second = -171
+    Number(first) > Number(second) should be(false)
+  }
+
+  "<" should
+    "7<1" in {
+    val first = 7
+    val second = 1
+    Number(first) < Number(second) should be(false)
+  }
+  it should "173 < 34" in {
+    val first = 173
+    val second = 34
+    Number(first) < Number(second) should be(false)
+  }
+
+  it should "173 < 171" in {
+    val first = 173
+    val second = 171
+    Number(first) < Number(second) should be(false)
+  }
+
+  it should "173 < -34" in {
+    val first = 173
+    val second = -34
+    Number(first) < Number(second) should be(false)
+  }
+
+  it should "-173 < 34" in {
+    val first = -173
+    val second = 34
+    Number(first) < Number(second) should be(true)
+  }
+
+  it should "-7<-1" in {
+    val first = -7
+    val second = -1
+    Number(first) < Number(second) should be(true)
+  }
+  it should "-173 < -34" in {
+    val first = -173
+    val second = -34
+    Number(first) < Number(second) should be(true)
+  }
+
+  it should "-173 < -171" in {
+    val first = -173
+    val second = -171
+    Number(first) < Number(second) should be(true)
+  }
+
+  "==" should
+    "7==7" in {
+    val first = 7
+    val second = 7
+    Number(first) == Number(second) should be(true)
+  }
+
+  it should "-7==-7" in {
+    val first = -7
+    val second = -7
+    Number(first) == Number(second) should be(true)
+  }
+
+  it should "7==-7" in {
+    val first = 7
+    val second = -7
+    Number(first) == Number(second) should be(false)
+  }
+
+  it should "-7==7" in {
+    val first = -7
+    val second = 7
+    Number(first) == Number(second) should be(false)
+  }
+
+  "!=" should
+    "7!=7" in {
+    val first = 7
+    val second = 7
+    Number(first) != Number(second) should be(false)
+  }
+
+  it should "-7!=-7" in {
+    val first = -7
+    val second = -7
+    Number(first) != Number(second) should be(false)
+  }
+
+  it should "7!=-7" in {
+    val first = 7
+    val second = -7
+    Number(first) != Number(second) should be(true)
+  }
+
+  it should "-7!=7" in {
+    val first = -7
+    val second = 7
+    Number(first) != Number(second) should be(true)
+  }
+
+  "*" should
+    "7*5=35" in {
+    val first = 7
+    val second = 5
+    Number(first) * Number(second) should be(Number(first * second))
+  }
+
+  it should "77*5=385" in {
+    val first = 77
+    val second = 5
+    Number(first) * Number(second) should be(Number(first * second))
+  }
+
+  it should "7777*5555=43201235" in {
+    val first = 7777
+    val second = 5555
+    Number(first) * Number(second) should be(Number(first * second))
+  }
+
+  it should "7777777777776546465L*555543432342332432L" in {
+    val first = 7777777777776546465L
+    val second = 555543432342332432L
+    Number(first) * Number(second) should be(Number("4320893362661901534495368583924452880".reverse, '+'))
+  }
+
+  it should "check for signs" in {
+    var first = -1
+    var second = 1
+    Number(first) * Number(second) should be(Number(first * second))
+    first = 1
+    second = -1
+    Number(first) * Number(second) should be(Number(first * second))
+    first = -1
+    second = -1
+    Number(first) * Number(second) should be(Number(first * second))
+  }
+
+  it should "pentest" in {
+    for (i <- 5000 to 5000) {
+      for (j <- -5000 to 5000)
+        Number(i) * Number(j) should be(Number(i * j))
+    }
+  }
+
+  "%" should "give right result" in {
+    var first = 100
+    var second = 15
+    Number(first) % Number(second) should be(Number(first % second))
+    first = 11000
+    second = 11000
+    Number(first) % Number(second) should be(Number(first % second))
+    first = 100
+    second = 200
+    Number(first) % Number(second) should be(Number(first % second))
+  }
+
+  it should "pentest" in {
+    for (i <- 5000 to 5000) {
+      for (j <- -5000 to 5000)
+        if (j != 0)
+          Number(i) % Number(j) should be(Number(i % j))
+    }
+  }
+
+  "/" should "100/3" in {
+    val first = 100
+    val second = 3
+    Number(first) / Number(second) should be((Number(first / second), Number(first % second)))
+  }
+
+  it should "pentest" in {
+    for (i <- 5000 to 5000) {
+      for (j <- -5000 to 5000)
+        if (j != 0)
+          Number(i) / Number(j) should be((Number(i / j), Number(i % j)))
+    }
+  }
+
+  it should "1/3" in {
+    val first = 1
+    val second = 3
+    Number(first) / Number(second) should be((Number(first / second), Number(first % second)))
+  }
+
+  it should "1030/32" in {
+    val first = 1030
+    val second = 32
+    Number(first) / Number(second) should be((Number(first / second), Number(first % second)))
+  }
+
+  it should "111/75" in {
+    val first = 111
+    val second = 75
+    Number(first) / Number(second) should be((Number(first / second), Number(first % second)))
+  }
+
+  it should "75/111" in {
+    val first = 75
+    val second = 111
+    Number(first) / Number(second) should be((Number(first / second), Number(first % second)))
+  }
+
+  "^" should "5^5" in {
+    val first = 5
+    val second = 5
+    Number(first) ^ Number(second) should be(Number(pow(first, second)))
+  }
+
+  it should "5^1" in {
+    val first = 5
+    val second = 1
+    Number(first) ^ Number(second) should be(Number(pow(first, second)))
+  }
+
+  it should "5^0" in {
+    val first = 5
+    val second = 0
+    Number(first) ^ Number(second) should be(Number(pow(first, second)))
+  }
+
+  def pow(a: Int, b: Int): Long = {
+    var res = 1L
+    for (i <- 1 to b) {
+      res = res * a
+    }
+    res
+  }
+
+  "sqrt" should "sqrt(1)" in {
+    val first = 1
+    val sqrt = 1
+    Number(first).sqrt() should be(Number(sqrt))
+  }
+
+  it should "sqrt (0)" in {
+    val first = 0
+    val sqrt = 0
+    Number(first).sqrt() should be(Number(sqrt))
+  }
+
+  it should "sqrt (16)" in {
+    val first = 16
+    val sqrt = 4
+    Number(first).sqrt() should be(Number(sqrt))
+  }
+
+  it should "sqrt (15)" in {
+    val first = 15
+    val sqrt = 3
+    Number(first).sqrt() should be(Number(sqrt))
+  }
+
+  it should "sqrt (17)" in {
+    val first = 17
+    val sqrt = 4
+    Number(first).sqrt() should be(Number(sqrt))
+  }
+
+  "-" should "pentest" in {
+    for (i <- -500 to 500) {
+      for (j <- -500 to 500) {
+        //println(Number(i) +" "+  Number(j))
+        (Number(i) - Number(j)) should be(Number(i - j))
+      }
+    }
+  }
+}
