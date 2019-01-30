f81fa5197efc9f2dd2b7fb60c01b3b1af7297bba
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2019-01-30 23:27:14 +0200

    [Algorithms]1.01 My Hashmap

--- algos/src/main/java/com/yermilov/algos/Main.java
+++ algos/src/main/java/com/yermilov/algos/Main.java
@@ -1,4 +1,19 @@
 package com.yermilov.algos;
 
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        int n = 1000;
+        for (int p = 3; p <= 6; p++) {
+            Random random = new Random(System.currentTimeMillis());
+            MyHashMap hashMap = new MyHashMap(n);
+            for (int i = 0; i < n; i++) {
+                hashMap.addNumber(Math.abs(random.nextInt()));
+            }
+            hashMap.setMinMax();
+            System.out.println("n = " + n + ", min = " + hashMap.getMin() + ", max = " + hashMap.getMax());
+            n*=10;
+        }
+    }
 }
--- algos/src/main/java/com/yermilov/algos/MyHashMap.java
+++ algos/src/main/java/com/yermilov/algos/MyHashMap.java
@@ -1,4 +1,57 @@
 package com.yermilov.algos;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
 public class MyHashMap {
+    private ArrayList<LinkedList<Integer>> storage;
+    private int p;
+    private final static Random RANDOM = new Random(System.currentTimeMillis());
+    private final static int a = RANDOM.nextInt(5333);
+    private final static int b = RANDOM.nextInt(1235);
+    private int min;
+    private int max;
+
+    public MyHashMap(int n) {
+        storage = new ArrayList<>();
+        for (int i = 0; i < n/100; i++) {
+            storage.add(new LinkedList<>());
+        }
+        p = n;
+        while (!isPrime(p)) p++;
+        min = n;
+    }
+
+    private boolean isPrime(int number) {
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+        for (int i = 3; 1L * i * i <= number; i += 2) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    private int hash(int number) {
+        return (int) (((long) a * number + b) % p)%storage.size();
+    }
+
+    public void addNumber(int number) {
+        storage.get(hash(number)).add(number);
+    }
+
+    public void setMinMax() {
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).size() > max) max = storage.get(i).size();
+            if (storage.get(i).size() < min) min = storage.get(i).size();
+        }
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
 }
